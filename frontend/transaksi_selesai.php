2d49c206a183a53644f2018e5e0e25bff1f9de3f
Naufa <izzahnaufalia@gmail.com>
2026-06-17 20:51:35 +0700

    fe konser2.php

--- frontend/transaksi_selesai.php
+++ frontend/transaksi_selesai.php
@@ -1,7 +1,9 @@
-if ($data['status_bayar'] == 'Pending') {
+
+
+if ($data['status_bayar'] == 'pending') {
     echo "Menunggu verifikasi admin";
 }
-elseif ($data['status_bayar'] == 'Lunas') {
+elseif ($data['status_bayar'] == 'paid') {
     echo "<a href='../backend/cetak_transaksi.php?id=".$data['id']."'>
             Download Bukti Transaksi
           </a>";
